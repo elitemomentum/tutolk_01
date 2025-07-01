19a2c3234711dc1d5e52b0104fdf56b03d62fc8f
Navneet Singh <navneet.singh@tci.email>
2025-07-01 18:05:12 +0530

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -1,11 +1,8 @@
 'use strict';
+const fetchJsonFile = await fetch('./api.json');
+const DID_API = await fetchJsonFile.json();
 
-const DID_API_KEY = process.env.DID_API_KEY;
-
-if (!DID_API_KEY || DID_API_KEY === '🤫') {
-  throw new Error('Please set your DID_API_KEY in environment variables.');
-}
-
+if (DID_API.key == '🤫') alert('Please put your api key inside ./api.json and restart..');
 
 const RTCPeerConnection = (
   window.RTCPeerConnection ||
